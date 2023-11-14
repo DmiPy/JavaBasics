12ce7586e7f3b16599b48dc139ca02794cd02cb8
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2023-11-14 11:23:07 +0100

    serial number program

--- src/aufgaben/Seriennummer.java
+++ src/aufgaben/Seriennummer.java
@@ -7,17 +7,20 @@ public class Seriennummer {
 	public static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int[] integers = new int[26];
+		
+		// alphabet is not used here, but good to see how letter++ functions
 		char[] alphabet = new char[26];
 		char letter = 'A';
-		for (int i = 0; i < 26; i++) {
-			integers[i] = i+1;
-		    alphabet[i] = letter++;
-		}
 		int sum = 0;
-		String number = new String();
+		for (int i = 0; i < 26; i++) {
+
+		    alphabet[i] = letter++;
+		} 
+	
+		
 		System.out.println("Enter your serial number. For Example: SB2420931622");
-		number = scan.nextLine();
+		String number = scan.nextLine();
+		
 		boolean ifLetters = Character.isLetter(number.charAt(0)) && Character.isLetter(number.charAt(1));
 		if (ifLetters && number.length() == 12) {
 			for(int i = 0; i < number.length(); i++) {
@@ -31,13 +34,18 @@ public class Seriennummer {
 					sum += value;
 				}
 			}
-			int remainder = sum % 9;
+			
+			// Variable 'sum' accumulates the numerical values of the letters and digits
+			// in the serial number to determine its validity based on the remainder
+			// when divided by 9.
+			int remainder = sum % 9; 
+			
+
             if (remainder == 7) {
                 System.out.println("The remainder is 7. The serial number is correct.");
             } else {
                 System.out.println("The remainder is not 7. The serial number is incorrect.");
             }
-			
 		}
 		else {
 			System.out.println("Invalid serial number format or length");
